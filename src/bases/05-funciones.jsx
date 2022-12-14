ed04be63c641ba08ae06d39f10e85efa0f0057d4
Yasiel Valdes <yasiel.valdes.azcuba.cu>
2022-12-14 17:10:48 -0500

    05 funciones

---
+++ src/bases/05-funciones.jsx
@@ -0,0 +1,35 @@
+const saludar = function (nombre) {
+    return `Hola, ${nombre}`;
+}
+console.log(saludar('Goku'));
+
+// funciones flechas 
+/// variable + numeros de argumentos y => y parentesis si se devuelve un objeto implicito
+const saludar2 = (nombre) => {
+    return `Hola, ${nombre}`;
+}
+console.log(saludar2('Vegeta'));
+
+const saludar3 = (nombre) => `Hola, ${nombre}`;
+console.log(saludar3('yasiel'));
+
+
+const getUser = () => (
+    {
+        uid: '827164781236847',
+        nombre: 'Yasiel',
+    }
+);
+
+const user = getUser();
+console.log(getUser());
+
+/// Ejercicio
+
+const getUsuarioActivo = (nombre) => ({
+    uid: "ABC567",
+    username: nombre
+});
+
+const activo = getUsuarioActivo("Yasiel Activo");
+console.log(activo)
